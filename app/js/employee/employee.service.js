fbc79d22ec11cc1efce660097cce0d3fb191a787
JoseJunio <zeh.junior@hotmail.com>
2018-02-10 05:45:41 -0200

    5° Commit Front End

--- app/js/employee/employee.service.js
+++ app/js/employee/employee.service.js
@@ -3,52 +3,27 @@ angular.
     .service("EmployeesService", ['$http', function($http){
         
         var that = this;
+		var teste = [];
 
         var api = "http://localhost:8080/api/webresources/employees";
         
         that.getEmployees = function(){
          // return that.employees;
-         return $http.get(api);
+         return teste;
         };
         
         that.save = function(employee){
             
-            /* Teste */
-            /*var employees = that.getEmployees();
-            var control   = false;
-            
-            for(var i=0; i < employees.length; i++){
-                if(employees[i].id == employee.id){
-                    
-                    var confirm = $mdDialog.confirm()
-                                    .title('Esse id já existe para um funcionário. Deseja alterá-lo ?')
-                                    .ariaLabel('Lucky day')
-                                    .targetEvent(ev)
-                                    .ok('Sim')
-                                    .cancel('Não');
-                    
-                    $mdDialog.show(confirm).then(function() {
-                         return $http.put(api + "/" + employee.id, employee);
-                      });
-                      control = true;
-                      break;
-                }
-            }
-            
-            if(!control){
-                return $http.post(api, employee);
-            }*/
-            
             if(employee.id){
-               return $http.put(api + "/" + employee.id, employee);
+               return teste;//$http.put(api + "/" + employee.id, employee);
             }else{
-               return $http.post(api, employee);
+               return teste; //$http.post(api, employee);
             }
           
         };
         
         that.delete = function(employee){
-          return $http.delete(api + "/" + employee.id);
+          return teste;//$http.delete(api + "/" + employee.id);
         };
                        
     }]);
--- app/js/locais/locais.service.js
+++ app/js/locais/locais.service.js
@@ -3,52 +3,25 @@ angular.
     .service("LocalService", ['$http', function($http){
         
         var that = this;
-
+		var teste = [];
+		
         var api = "http://localhost:8080/api/webresources/locais";
         
         that.getLocais = function(){
-         // return that.employees;
-         return $http.get(api);
+         return teste;//$http.get(api);
         };
         
         that.save = function(local){
-            
-            /* Teste */
-            /*var employees = that.getEmployees();
-            var control   = false;
-            
-            for(var i=0; i < employees.length; i++){
-                if(employees[i].id == employee.id){
-                    
-                    var confirm = $mdDialog.confirm()
-                                    .title('Esse id já existe para um funcionário. Deseja alterá-lo ?')
-                                    .ariaLabel('Lucky day')
-                                    .targetEvent(ev)
-                                    .ok('Sim')
-                                    .cancel('Não');
-                    
-                    $mdDialog.show(confirm).then(function() {
-                         return $http.put(api + "/" + employee.id, employee);
-                      });
-                      control = true;
-                      break;
-                }
-            }
-            
-            if(!control){
-                return $http.post(api, employee);
-            }*/
-            
             if(local.id){
-               return $http.put(api + "/" + local.id, local);
+               return teste;//$http.put(api + "/" + local.id, local);
             }else{
-               return $http.post(api, local);
+               return teste;//$http.post(api, local);
             }
           
         };
         
         that.delete = function(local){
-          return $http.delete(api + "/" + local.id);
+          return teste;//$http.delete(api + "/" + local.id);
         };
                        
     }]);
