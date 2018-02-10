153de25223d51c2955561deccab5af53b201a8ce
JoseJunio <zeh.junior@hotmail.com>
2018-02-10 06:26:25 -0200

    p

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,11 @@
 var gulp = require('gulp');
 var connect = require('gulp-connect');
 var inject = require('gulp-inject');
-var include = require("gulp-include");;
+var include = require("gulp-include");
+var livereload = require('gulp-livereload');
 
 gulp.task('index', function() {
-    var target = gulp.src('app/main.html');
+    var target = gulp.src('app/index.html');
    
     return target.pipe();
 
@@ -36,6 +37,7 @@ gulp.task('css', function() {
 });
 
 gulp.task('watch', function() {
+    livereload.listen(5000);
     gulp.watch(['./app/**/*.html'], ['html']);
     gulp.watch(['./app/**/*.js'], ['js']);
     gulp.watch(['./app/**/*.css'], ['css']);
